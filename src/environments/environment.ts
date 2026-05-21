685f7140eab8d6a5aab12962aac3886e74b7151f
Xmiguelonn <145677972+Xmiguelonn@users.noreply.github.com>
2026-05-21 20:30:05 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: true,
+    production: false,
     stripePublicKey: 'pk_test_51TRDVUEYyMQaFZmRkv3rvgur8Qk5HWPb3UxOQBiIo2QvATCOn6oabtR9lfUVXl41QjZ6zLdsgPiiqNjg5oZwdT9M00kbfFv565',
     apiUrl: 'http://localhost:8000/api',
 };
